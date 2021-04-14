484c5fc67b616f833090651d51cb3329bfe26519
youseon1028 <youseoun1028@naver.com>
2021-04-14 15:40:33 +0900

    heroku upload

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+#from django.contrib.auth.models import User
+from photo.models import User # 새로운 필드 추가한 User 사용
+from django import forms
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'gender', 'birthdate']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Passwords not matched!')
+        return cd['password2']
---
+++ photo/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+
+
+GENDER_C = (
+    ("선택안함", "선택안함"),
+    ('여성', '여성'),
+    ('남성', '남성'),
+)
+
+class User(AbstractUser):
+    gender = models.CharField(max_length=10, choices=GENDER_C, default='N')
+    birthdate = models.DateField(null=True, blank=True)
+
+class Photo(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_photos')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', default='photos/no_image.png')
+    text = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated']
+
+    def __str__(self):
+        return self.author.username + " " + self.created.strftime("%Y-%m-%d%H:%M:%S")
+
+    def get_absolute_url(self):
+        return reverse('photo:photo_detail', args=[str(self.id)])
